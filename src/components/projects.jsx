165cef8166473626dab1b1db14e4460b36cb75aa
carlosmejia10 <117192998+carlosmejia10@users.noreply.github.com>
2024-03-03 16:04:23 -0500

    finished the carousels, adding some information in projects and certificates

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,73 +1,85 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import img1 from "../img/img1.jpg";
 import img2 from "../img/img2.jpg";
 import img3 from "../img/img3.jpg";
 import img4 from "../img/img4.jpg";
+import githubImage from "../img/github 2.png";
 
-const Projects = () => {
-  const [active, setActive] = useState(0);
+const Proyects = () => {
+  const [activeSlide, setActiveSlide] = useState(0);
 
   const sliderContent = [
     {
       img: img1,
-      name: "API",
+      description: "Description for Image 1",
+      link: "a",
     },
     {
       img: img2,
-      name: "Store-app",
+      description: "Description for Image 2",
+      link: "",
     },
     {
       img: img3,
-      name: "Landing-page",
+      description: "Description for Image 3",
+      link: "",
     },
     {
       img: img4,
-      name: "Financial-app",
+      description: "Description for Image 4",
+      link: "",
     },
   ];
 
   const Slide = (type) => {
-    let local;
     if (type === "next") {
-      local = active + 1;
-      sliderContent.length - 1 < local
-        ? setActive(local / sliderContent.length - 1)
-        : setActive(local);
-    }
-    if (type === "prev") {
-      local = active - 1;
-      local < 0 ? setActive(sliderContent.length - 1) : setActive(local);
+      setActiveSlide((activeSlide + 1) % sliderContent.length);
+    } else if (type === "prev") {
+      const newActiveSlide =
+        (activeSlide - 1 + sliderContent.length) % sliderContent.length;
+      setActiveSlide(newActiveSlide);
     }
   };
 
   return (
     <div>
-      <div className="rounded-xl relative shadow-lg overflow-hidden w-[600px] h-[400px] mx-auto">
-        <div className="w-[600px] h-[400px] relative">
-          {sliderContent.map((slide, i) => {
-            return (
-              <img
-                src={slide.img}
-                key={i}
-                alt="slideImg"
-                className={`h-full w-full absolute object-cover inset-0 duration-[2s] ease-out transition-[clip-path] ${
-                  i === active ? "clip-visible" : "hidden"
-                }`}
-              />
-            );
-          })}
+      <div className="flex">
+        <div className="relative rounded-xl shadow-xl overflow-hidden w-[600px] h-[400px] mt-3 left-8">
+          <img
+            src={sliderContent[activeSlide].img}
+            alt="slideImg"
+            className="h-full w-full absolute object-cover inset-0 transition duration-[0.75s]"
+          />
+          <div className="absolute inset-y-0 flex items-center justify-between w-full px-4">
+            <button id="back" className="z-10" onClick={() => Slide("prev")}>
+              <ion-icon name="chevron-back-outline" size="large"></ion-icon>
+            </button>
+            <button
+              id="forward"
+              className="z-10 right-0"
+              onClick={() => Slide("next")}
+            >
+              <ion-icon name="chevron-forward-outline" size="large"></ion-icon>
+            </button>
+          </div>
         </div>
-        <div className="absolute inset-y-0 flex items-center justify-between w-full px-4">
-          <button id="back" className="z-10" onClick={() => Slide("prev")}>
-            <ion-icon name="chevron-back-outline" size="large"></ion-icon>
-          </button>
-          <button id="foward" className="z-10 right-0" onClick={() => Slide("next")}>
-            <ion-icon name="chevron-forward-outline" size="large"></ion-icon>
-          </button>
+        <div className="bg-gray-400/50 p-4 rounded-xl shadow-xl w-96 h-[300px] fixed mt-[165px] transform -translate-y-1/2 right-60">
+          <div className="text-lg font-medium">
+            {sliderContent[activeSlide].description}
+          </div>
+          <div className="text-gray-500">
+            More information about the image...
+          </div>
+        </div>
+        <div className="bg-gray-400/50 p-4 justify-center items-center flex rounded-full shadow-xl w-96 h-20 fixed top-[572px] transform -translate-y-1/2 right-60">
+          <div className="text-md font-sm relative flex bg-gray-200/40 justify-center items-center  rounded-full  w-80 h-10 transform transition-transform duration-300 hover:scale-110 ">
+            <img src={githubImage} className="w-6 absolute mr-[180px]" />
+            <a href={sliderContent[activeSlide].link} className="ml-5">Github Repository</a>
+          </div>
         </div>
       </div>
     </div>
   );
 };
 
-export default Projects;
+export default Proyects;
