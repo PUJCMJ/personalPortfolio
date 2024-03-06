c01601e7b4b29739ce3189f3d647f102240fe682
carlosmejia10 <117192998+carlosmejia10@users.noreply.github.com>
2024-03-05 20:57:52 -0500

    Some 'about component implementations'

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,9 +1,11 @@
 const About = () => {
-    return(
-        <>
-            About me
-        </>
-    )
-}
-
-export default About;
+    return (
+      <div className=" border-slate-100 border-2xl bg-blue-500/10 rounded-3xl items-center shadow-xl w-[700px] h-[400px] relative flex p-10">
+        <div className=" absolute top-12 mr-10 ml-5 text-slate-300/70">
+          More information about the image...bdghsadcsagvdsaghdcsaghdfsafgdsafhdjascddvbsadsadsa jsbdhabdhsuabd
+        </div>
+      </div>
+    );
+  };
+  
+  export default About;
